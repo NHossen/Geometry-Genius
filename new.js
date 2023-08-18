8a173983208e4c76abb68117f273735eb9962db7
NHossen <naeemhossen2509@gmail.com>
2023-08-19 00:18:10 +0400

    dynamic html added for calculation all of geometry

--- new.js
+++ new.js
@@ -1,33 +1,42 @@
 // Tryangel
 function calculateTriangleArea() {
-    const getBaseValue=getElementByIdfunction('triangel-base','value');
+    const getBaseValue=getElementByIdfunction('triangel-base');
 //console.log(getBaseValue);
-const getHeightValue=getElementByIdfunction('triangel-height','value');
+const getHeightValue=getElementByIdfunction('triangel-height');
 //console.log(getHeightValue);
 const areaOfTriangel=0.5*getBaseValue*getHeightValue;
 setInnerText('triangel-area', areaOfTriangel);
+
+addToCalculationEntry('Tyaggel',areaOfTriangel);
 }
 
 //Rectangel
 function calculateRectangleArea(){
-    const getWidthValue=getElementByIdfunction('rectangle-width','value');
-    const getLengthValue=getElementByIdfunction('rectangle-length','value');
+    const getWidthValue=getElementByIdfunction('rectangle-width');
+    const getLengthValue=getElementByIdfunction('rectangle-length');
     const areaOfRectangel=getWidthValue*getLengthValue;
     setInnerText('rectangle-area',areaOfRectangel);
+
+    addToCalculationEntry('Rectangel',areaOfRectangel);
     
 }
 //parallelogram
 function calculateParallelogramArea(){
-    const getBaseValue=getElementByIdfunction('parallelogram-base','value');
-    const getHeightValue=getElementByIdfunction('parallelogram-height','value');
+    const getBaseValue=getElementByIdfunction('parallelogram-base');
+    const getHeightValue=getElementByIdfunction('parallelogram-height');
     const areaOfParallelogram=getBaseValue*getHeightValue;
     setInnerText('parallelogram-area',areaOfParallelogram);
+
+    //add to calculation entry
+
+    addToCalculationEntry('Paralleogram',areaOfParallelogram);
+
 }
 
 //rhombus
 function calculateRhombusArea(){
-    const getValueRombosh=getElementByIdfunction('diagonal-base','value');
-    const geDiagonal=getElementByIdfunction('diagonal-height','value');
+    const getValueRombosh=getElementByIdfunction('diagonal-base');
+    const geDiagonal=getElementByIdfunction('diagonal-height');
     const areaOfRhombosh=0.5*getValueRombosh*geDiagonal;
     setInnerText('rhombus-area',areaOfRhombosh);
 }
@@ -35,18 +44,19 @@ function calculateRhombusArea(){
 //pentagoan
 
 function calculatePentagonArea(){
-    const getValuePentagon=getElementByIdfunction('Pentagon-base','value');
-    const getvalueB=getElementByIdfunction('Pentagon-height','value');
+    const getValuePentagon=getElementByIdfunction('Pentagon-base');
+    const getvalueB=getElementByIdfunction('Pentagon-height');
     const areaOfPentagon=0.5*getValuePentagon*getvalueB;
     setInnerText('Pentagon-area',areaOfPentagon);
 }
 
 //ellipse
 function calculateEllipseArea(){
-    const getValueElipse=getElementByIdfunction('Ellipse-a','value');
-    const getValueelipseB=getElementByIdfunction('Ellipse-b','value');
+    const getValueElipse=getElementByIdfunction('Ellipse-a');
+    const getValueelipseB=getElementByIdfunction('Ellipse-b');
     const areaOfElipseArea=3.1415*getValueElipse*getValueelipseB;
-    setInnerText('Ellipse-area',areaOfElipseArea);
+    const areaTwoDecimal=areaOfElipseArea.toFixed(2);
+    setInnerText('Ellipse-area',areaTwoDecimal);
     
 
 }
@@ -54,7 +64,7 @@ function calculateEllipseArea(){
 
 
 // Get value by function
-function getElementByIdfunction(elementID,valueID){
+function getElementByIdfunction(elementID){
     const elementValue=document.getElementById(elementID);
     const value=elementValue.value;
     const valueParseIntoFlot=parseFloat(value);
@@ -66,5 +76,24 @@ function setInnerText(elementID, newText) {
     element.innerText = newText;
 }
 
+//add to calculation entry
+/*
+ 1.get the element where you want to add the dynamic html
+ 2.Create an element you want to add
+ 3.if needed add some class
+ 4.set inner html it could be dynamic template string
+ 5. add the create element by appenchild with paarent chaild
+*/
+function addToCalculationEntry(areaType,area){
+    console.log(areaType+ ' ' + area)
+    const calculationEntry=document.getElementById('calculation-entry');//step 1
+
+       const count=calculationEntry.childElementCount;
+
+
+    const p=document.createElement('p');//step 2
+    p.innerHTML=`${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button">Convert</button>`             //areaType+ ' ' + area //area;
+    calculationEntry.appendChild(p);
+}
 
 
