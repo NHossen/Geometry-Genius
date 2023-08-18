805663eff54078ce5451f900fbf27ffa72d95ece
NHossen <naeemhossen2509@gmail.com>
2023-08-18 22:00:02 +0400

    get elipse area by function

--- new.js
+++ new.js
@@ -1,3 +1,4 @@
+// Tryangel
 function calculateTriangleArea() {
     const getBaseValue=getElementByIdfunction('triangel-base','value');
 //console.log(getBaseValue);
@@ -7,13 +8,47 @@ const areaOfTriangel=0.5*getBaseValue*getHeightValue;
 setInnerText('triangel-area', areaOfTriangel);
 }
 
-
+//Rectangel
 function calculateRectangleArea(){
     const getWidthValue=getElementByIdfunction('rectangle-width','value');
     const getLengthValue=getElementByIdfunction('rectangle-length','value');
     const areaOfRectangel=getWidthValue*getLengthValue;
     setInnerText('rectangle-area',areaOfRectangel);
     
+}
+//parallelogram
+function calculateParallelogramArea(){
+    const getBaseValue=getElementByIdfunction('parallelogram-base','value');
+    const getHeightValue=getElementByIdfunction('parallelogram-height','value');
+    const areaOfParallelogram=getBaseValue*getHeightValue;
+    setInnerText('parallelogram-area',areaOfParallelogram);
+}
+
+//rhombus
+function calculateRhombusArea(){
+    const getValueRombosh=getElementByIdfunction('diagonal-base','value');
+    const geDiagonal=getElementByIdfunction('diagonal-height','value');
+    const areaOfRhombosh=0.5*getValueRombosh*geDiagonal;
+    setInnerText('rhombus-area',areaOfRhombosh);
+}
+
+//pentagoan
+
+function calculatePentagonArea(){
+    const getValuePentagon=getElementByIdfunction('Pentagon-base','value');
+    const getvalueB=getElementByIdfunction('Pentagon-height','value');
+    const areaOfPentagon=0.5*getValuePentagon*getvalueB;
+    setInnerText('Pentagon-area',areaOfPentagon);
+}
+
+//ellipse
+function calculateEllipseArea(){
+    const getValueElipse=getElementByIdfunction('Ellipse-a','value');
+    const getValueelipseB=getElementByIdfunction('Ellipse-b','value');
+    const areaOfElipseArea=3.1415*getValueElipse*getValueelipseB;
+    setInnerText('Ellipse-area',areaOfElipseArea);
+    
+
 }
 
 
